e2ed723c2c4510b610a7b47efd903c56a25aa912
AleksTimofeev <timoha144a@gmail.com>
2022-09-20 02:22:48 +0300

    refactor: change API login request

--- src/api/cards-api.ts
+++ src/api/cards-api.ts
@@ -1,22 +1,36 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-    // baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/' ,
-    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:7542/2.0/' : 'https://neko-back.herokuapp.com/2.0/',
-    withCredentials: true,
+  // baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/' ,
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:7542/2.0/' : 'https://neko-back.herokuapp.com/2.0/',
+  withCredentials: true,
 })
 
 export const authAPI = {
-    login() {
-        return instance.post('auth/login',{
-            email: "nya-admin@nya.nya",
-            password: "1qazxcvBG",
-            rememberMe: false })
-    },
-    me() {
-        return ''
-    },
-    logout() {
-        return ''
-    }
+  login(email: string, password: string, rememberMe: boolean) {
+    return instance.post<UserType>('auth/login', {email, password, rememberMe})
+      .then(res => res.data)
+  },
+  me() {
+    return ''
+  },
+  logout() {
+    return ''
+  }
+}
+
+export type UserType = {
+  avatar: string
+  created: string
+  email: string
+  isAdmin: boolean
+  name: string
+  publicCardPacksCount: number
+  rememberMe: boolean
+  token: string
+  tokenDeathTime: number
+  updated: string
+  verified: boolean
+  __v: number
+  _id: string
 }
