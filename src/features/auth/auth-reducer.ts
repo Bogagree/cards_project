d5813506d036c4bb7291c5e3253f9ed9e120de0a
AleksTimofeev <timoha144a@gmail.com>
2022-09-20 02:25:52 +0300

    refactor: change authReducer

--- src/features/auth/auth-reducer.ts
+++ src/features/auth/auth-reducer.ts
@@ -1,25 +1,43 @@
-import {Dispatch} from "redux";
-import {authAPI} from "../../api/cards-api";
+import {authAPI, UserType} from "../../api/cards-api";
+import {AppThunkType} from "../../app/store";
 
-type ActionType = {
-    type: string
+export type AuthActionType = ReturnType<typeof loginAC>
+
+type initialStateType = {
+  isLogged: boolean
+} & UserType
+
+const initialState: initialStateType = {
+  isLogged: false,
+  avatar: '',
+  created: '',
+  email: '',
+  isAdmin: false,
+  name: '',
+  publicCardPacksCount: 0,
+  rememberMe: false,
+  token: '',
+  tokenDeathTime: 0,
+  updated: '',
+  verified: false,
+  __v: 0,
+  _id: '',
 }
 
-const initialState = {}
-
-export const authReducer = (state = initialState, action: ActionType) => {
-    switch (action.type) {
-        default:
-            return state
-    }
+export const authReducer = (state = initialState, action: AuthActionType) => {
+  switch (action.type) {
+    case "LOGIN": return {...state, ...action.userData, isLogged: true}
+    default:
+      return state
+  }
 };
 
+const loginAC = (userData: UserType) => ({type: 'LOGIN', userData})
+export const loginTC = (email: string, password: string, rememberMe: boolean): AppThunkType => async dispatch => {
+  try {
+    const res = await authAPI.login(email, password, rememberMe)
+    dispatch(loginAC(res))
+  } catch (e) {
 
-export const loginTC = () => async (dispatch: Dispatch) => {
-    try {
-        const res = await authAPI.login()
-        console.log(res.data)
-    } catch (e) {
-
-        }
-    }
+  }
+}
