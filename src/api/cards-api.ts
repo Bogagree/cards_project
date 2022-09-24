7c5a74696dedf9b10622b926bc01a7308e0290ca
AleksTimofeev <timoha144a@gmail.com>
2022-09-24 22:37:48 +0300

    refactor: add response types for auth-reducer

--- src/api/cards-api.ts
+++ src/api/cards-api.ts
@@ -1,58 +1,72 @@
-import axios from 'axios'
-import {RegistrationDataType} from "../features/auth/auth-reducer";
+import axios, {AxiosResponse} from 'axios'
+import {LoginDataType, RegistrationDataType} from "../features/auth/auth-reducer";
 
 export const instance = axios.create({
-    // baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/' ,
-    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:7542/2.0/' : 'https://neko-back.herokuapp.com/2.0/',
-    withCredentials: true,
+  // baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/' ,
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:7542/2.0/' : 'https://neko-back.herokuapp.com/2.0/',
+  withCredentials: true,
 })
 
 export const authAPI = {
-    login(email: string, password: string, rememberMe: boolean) {
-        return instance.post<UserType>('auth/login', {email, password, rememberMe})
-            .then(res => res.data)
-    },
-    authMe() {
-        return instance.post<UserType>('/auth/me', {})
-            // .then(res => res.data)
-    },
-    updateUser(name: string) {
-        return instance.put('/auth/me', {name: name})
-    },
-    logout() {
-        return instance.delete('auth/me')
-    },
-    registration(data: RegistrationDataType) {
-        return instance.post('auth/register', {...data})
-    },
-    ping() {
-        return instance.get('/ping')
-    },
-    sendPingData() {
-        return instance.post('/ping', {frontTime: Date.now()})
-    }
+  login(loginData: LoginDataType) {
+    return instance.post<LoginDataType, AxiosResponse<UserType>>('auth/login', loginData)
+      .then(res => res.data)
+  },
+  authMe() {
+    return instance.post<UserType>('/auth/me', {})
+    // .then(res => res.data)
+  },
+  updateUser(name: string) {
+    return instance.put<{ name: string }, AxiosResponse<{ updatedUser: UserType }>>('/auth/me', {name: name})
+  },
+  logout() {
+    return instance.delete<{ info: string }>('auth/me')
+  },
+  registration(data: RegistrationDataType) {
+    return instance.post<RegistrationDataType, AxiosResponse<RegistrationResponseType>>('auth/register', {...data})
+  },
+  sendPingData() {
+    return instance.post<PingResponseType>('/ping', {frontTime: Date.now()})
+  }
 }
 
-export const packAPI = {
+export const packAPI = {}
 
-}
+export const cardAPI = {}
 
-export const cardAPI = {
-
+export type PingResponseType = {
+  ping: number
+  backTime: number
+  frontTime: number
+  info: string
 }
 
 export type UserType = {
-    avatar: string
+  avatar: string
+  created: string
+  email: string
+  isAdmin: boolean
+  name: string
+  publicCardPacksCount: number
+  rememberMe: boolean
+  token: string
+  tokenDeathTime: number
+  updated: string
+  verified: boolean
+  __v: number
+  _id: string
+}
+export type RegistrationResponseType = {
+  addedUser: {
     created: string
     email: string
     isAdmin: boolean
-    name: string
+    name:string
     publicCardPacksCount: number
-    rememberMe: boolean
-    token: string
-    tokenDeathTime: number
+    rememberMe:boolean
     updated: string
     verified: boolean
     __v: number
     _id: string
+  }
 }
