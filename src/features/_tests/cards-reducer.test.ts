a3fd7a0f75977d794a589217f48a9c10b3f5676d
AleksTimofeev <timoha144a@gmail.com>
2022-09-29 01:36:24 +0300

    feat: add test cards-reducer

---
+++ src/features/_tests/cards-reducer.test.ts
@@ -0,0 +1,69 @@
+import {cardsReducer, CardsStateType, setCardsAC} from "../cards/cards-reducer";
+
+let initialState: CardsStateType
+
+beforeEach(() => {
+  initialState = {
+    cards: [
+      {
+      _id: '11001',
+      cardsPack_id: '22001',
+      user_id: '33001',
+      answer: 'answer 001',
+      question: 'question 001',
+      grade: 0,
+      shots: 0,
+      comments: 'comments 001',
+      type: 'string',
+      rating: 1,
+      more_id: '11001',
+      created: '01.01.01',
+      updated: '02.01.01',
+      __v: 0,
+    },
+    {
+      _id: '11002',
+      cardsPack_id: '22001',
+      user_id: '33001',
+      answer: 'answer 002',
+      question: 'question 002',
+      grade: 0,
+      shots: 0,
+      comments: 'comments 002',
+      type: 'string',
+      rating: 1,
+      more_id: '11002',
+      created: '01.02.01',
+      updated: '02.02.01',
+      __v: 0,
+    },
+    {
+      _id: '11003',
+      cardsPack_id: '22001',
+      user_id: '33001',
+      answer: 'answer 003',
+      question: 'question 003',
+      grade: 0,
+      shots: 0,
+      comments: 'comments 003',
+      type: 'string',
+      rating: 1,
+      more_id: '11003',
+      created: '01.03.01',
+      updated: '02.03.01',
+      __v: 0,
+    },
+    ]
+  }
+})
+
+test('set cards', () => {
+  const state = {cards: []}
+  const newState = cardsReducer(state, setCardsAC(initialState.cards))
+  expect(newState.cards.length).toBe(3)
+})
+test('update cards data', () => {
+  const newCardsData = [initialState.cards[0], initialState.cards[1]]
+  const newState = cardsReducer(initialState, setCardsAC(newCardsData))
+  expect(newState.cards.length).toBe(2)
+})
